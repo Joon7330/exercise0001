0ab84dc2d022bd11334a0f98bf9dcb214cbeae24
Noah <jjhk7330@naver.com>
2023-03-30 14:01:14 +0900

    Update: esLint

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { ErrorWrapper, Layout, Header } from './components';
 
 const App: React.FunctionComponent = () => {
-  return (
-    <BrowserRouter>
-      <ErrorWrapper>
-        <Header />
-        <Layout />
-      </ErrorWrapper>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <ErrorWrapper>
+                <Header />
+                <Layout />
+            </ErrorWrapper>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/Sample.tsx
+++ src/pages/Sample.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const SamplePage: React.FunctionComponent = () => {
-    
+
     return (
         <>
             <div>SamplePage</div>
